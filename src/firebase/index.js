4fbfee499d8f01c600e14d4f08ecf957569373d4
Jordan Albert <jordan.albert@tgbbrands.com>
2019-05-09 10:41:47 -0500

    second commit

---
+++ src/firebase/index.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC4vrarrG1C9THLgNam5320lga5qeb8DmE",
+    authDomain: "meal-prep-cf593.firebaseapp.com",
+    databaseURL: "https://meal-prep-cf593.firebaseio.com",
+    projectId: "meal-prep-cf593",
+    storageBucket: "meal-prep-cf593.appspot.com",
+    messagingSenderId: "279932931178",
+    appId: "1:279932931178:web:511d67e5ba1dba02"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
